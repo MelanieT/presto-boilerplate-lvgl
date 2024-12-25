debe7576d100bac282cff14bc353277d1d1812f1
Melanie <melanie@t-data.com>
2024-12-25 22:13:36 +0000

    Initial commit, working version

--- README.md
+++
@@ -1,3 +0,0 @@
-# Presto C++ Boilerplate
-
-A basic project for getting started with C++ on the RP2350-powered Presto.
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
-#include "libraries/pico_graphics/pico_graphics.hpp"
-#include "libraries/pico_vector/pico_vector.hpp"
-#include "drivers/st7701/st7701.hpp"
-
-#include "ff.h"
-
-#include "pico/multicore.h"
-#include "pico/sync.h"
-
-#include "hardware/dma.h"
+#include "st7701.hpp"
+#include "lvgl.h"
+#include "SquareLineWorkspace/SquareLine_Project/ui/ui.h"
+#include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "cstdio"
+#include "pimoroni_i2c.hpp"
+#include "drivers/ft6x36/ft6x36.h"
 
 using namespace pimoroni;
-
-FATFS fs;
-FIL fil;
-FIL audio_file;
-FRESULT fr;
 
 #define FRAME_WIDTH 240
 #define FRAME_HEIGHT 240
@@ -29,11 +22,14 @@ uint16_t back_buffer[FRAME_WIDTH * FRAME_HEIGHT];
 uint16_t front_buffer[FRAME_WIDTH * FRAME_HEIGHT];
 
 ST7701* presto;
-PicoGraphics_PenRGB565* display;
-PicoVector *vector;
 
+void dummy_read(lv_indev_t *indev, lv_indev_data_t *data)
+{
+    data->state = LV_INDEV_STATE_RELEASED;
+}
 
-int main() {
+int main()
+{
     set_sys_clock_khz(240000, true);
     stdio_init_all();
 
@@ -41,45 +37,68 @@ int main() {
     gpio_put(LCD_CS, 1);
     gpio_set_dir(LCD_CS, 1);
 
-    sleep_ms(5000);
     printf("Hello\n");
 
-    fr = f_mount(&fs, "", 1);
-    if (fr != FR_OK) {
-      printf("Failed to mount SD card, error: %d\n", fr);
-      return 0;
-    }
-
-    printf("SD card mounted!");
-
-    presto = new ST7701(FRAME_WIDTH, FRAME_HEIGHT, ROTATE_0, SPIPins{spi1, LCD_CS, LCD_CLK, LCD_DAT, PIN_UNUSED, LCD_DC, BACKLIGHT}, back_buffer);
-    display = new PicoGraphics_PenRGB565(FRAME_WIDTH, FRAME_HEIGHT, front_buffer);
-    vector = new PicoVector(display);
+    presto = new ST7701(FRAME_WIDTH, FRAME_HEIGHT, ROTATE_0,
+                        SPIPins{spi1, LCD_CS, LCD_CLK, LCD_DAT, PIN_UNUSED, LCD_DC, BACKLIGHT}, back_buffer);
 
     printf("Init: ...");
     presto->init();
     printf("Init: OK\n");
 
-    printf("Font: ...");
-    if(vector->set_font("/marker-high.af", 30)) {
-      printf("Font: OK\n");
-    } else {
-      printf("Font: Fail!\n");
-    }
+    static I2C i2c(30, 31, 100000);
+    static FT6X36 touch(&i2c, 0x48);
 
-    sleep_ms(1000);
+    /*************************** LVGL ***********************************/
 
-    pp_mat3_t t = pp_mat3_identity();
-    pp_mat3_translate(&t, 50, 50);
+    lv_init();
 
-    while (true) {
-        display->set_pen(0);
-        display->clear();
-        display->set_pen(65535);
-        display->rectangle({20, 20, 200, 200});
-        display->set_pen(0);
-        vector->text("Hello World, how are you today?", 240, 240, &t);
-        display->text("Hello", {30, 30}, 240);
-        presto->update(display);
+    printf("LV Init done\r\n");
+
+    lv_display_t *display = lv_display_create(FRAME_WIDTH, FRAME_HEIGHT);
+
+    printf("LV display created\r\n");
+
+    lv_display_set_buffers(display, front_buffer, back_buffer, sizeof(front_buffer), LV_DISPLAY_RENDER_MODE_DIRECT);
+
+    printf("LV buffers created and set\r\n");
+
+    lv_display_set_flush_cb(display, [](lv_display_t *display, const lv_area_t *area, uint8_t *px_map) {
+        for (int i = area->y1 * FRAME_WIDTH * 2 ; i <= area->y2 * FRAME_WIDTH * 2 ; i += FRAME_WIDTH * 2)
+        {
+            for (int x = area->x1 * 2 ; x <= area->x2 * 2 ; x += 2)
+            {
+                uint8_t tmp = px_map[i + x];
+                px_map[i + x] = px_map[i + x +1];
+                px_map[i + x + 1] = tmp;
+            }
+        }
+        presto->set_framebuffer((uint16_t *) px_map);
+        lv_display_flush_ready(display);
+    });
+
+    printf("LV flush callback set\r\n");
+
+//    xpt2046_init();
+
+    lv_indev_t *indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+    lv_indev_set_read_cb(indev, [](lv_indev_t *drv, lv_indev_data_t *data) {
+        touch.ft6x36_read(drv, data);
+    });
+
+    /********************************************************************/
+
+    printf("Init UI\r\n");
+
+    ui_init();
+
+    printf("Entering main loop\r\n");
+
+    while (true)
+    {
+        lv_timer_handler();
+        sleep_ms(1);
+        lv_tick_inc(1);
     }
 }
